846aa8b511c659c93435ebb3ad59df9608e70f4f
floromo <flormmmp@gmail.com>
2024-09-20 10:00:30 -0700

    completed assignment

--- Main.java
+++ Main.java
@@ -1,13 +1,29 @@
+import java.lang.Math;
+
 public class Main
 {
   public static void main(String[] args)
   {
-    // test your functions here to make sure they work
+    distance(0,5,0,0);
+    quadraticAdd(1.0, 4.0, 2.0);
+    quadraticSub(1.0, 4.0, 2.0);
   }
 
-  // Implement distance formula method here
+  // ----- Distance Formula -----
+  private static void distance(int x1, int y1, int x2, int y2) {
+    double D = Math.sqrt((double)(x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+    System.out.println(D);
+  }
 
-  // implement one of your quadratic formula methods here
+  // ----- Quadratic Formula 1 -----
+  private static void quadraticAdd(double a, double b, double c){
+    double x = ((-1*b + Math.sqrt(b*b - 4*a*c))/2*a);
+    System.out.println(x);
+  }
 
-  // implement the other quadratic formula here
+  // ----- Quadratic Formula 2 -----
+  private static void quadraticSub(double a, double b, double c){
+    double x = ((-1*b - Math.sqrt(b*b - 4*a*c))/2*a);
+    System.out.println(x);
+  }
 }
